c6d53f072564b11917efebab701c5df1b9c39771
kamelot88 <VasilchenkoOV1988@gmail.com>
2021-10-05 00:08:22 +0300

    finish struktur

---
+++ control.js
@@ -0,0 +1,22 @@
+
+
+
+// const colectionQuestions = document.querySelectorAll("body > div > div.wrap-question > div.question");
+
+
+// function sayHi(elem) {
+//     elem.style.display = "none";
+//   }
+
+// for (let elemColection of colectionQuestions) {
+    
+//     elemColection.style.display = "block";
+//     setInterval(sayHi, 5000, elemColection); 
+
+
+
+
+// }
+
+
+
