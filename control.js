fa33f6302e6c3f66f69e09907a5a907951e2d1ca
kamelot88 <VasilchenkoOV1988@gmail.com>
2021-10-08 18:24:01 +0300

    feat: timer

--- control.js
+++ control.js
@@ -1,22 +1,66 @@
 
+//? скрипт переключения блоков вопросов
+
+const colectionQuestions = document.querySelectorAll(".question"); // получаю коллекцию элементов
 
 
-// const colectionQuestions = document.querySelectorAll("body > div > div.wrap-question > div.question");
+let i = 0;      // индекс елемента в массиве
+
+function myLoop () {
+    colectionQuestions[i].style.display = 'block';
+    setTimeout(function () {
+    colectionQuestions[i].style.display = 'none';
+      i++; // увеличиваю инднекс на 1
+      if (i < 10) {
+        myLoop ();// пока индекс мень 10 выполняю функции
+      } else {
+        document.location.href = "index.html";
+      }
+   }, 30000); // устанавливаю интервал в милисекундах
+}
 
 
-// function sayHi(elem) {
-//     elem.style.display = "none";
-//   }
+// function startTimer(duration, displayTime) {
+//     var timer = duration, seconds;
+//     setInterval(function () {
 
-// for (let elemColection of colectionQuestions) {
-    
-//     elemColection.style.display = "block";
-//     setInterval(sayHi, 5000, elemColection); 
+//         seconds = parseInt(timer % 60, 10);
 
+//         displayTime.textContent = seconds;
 
-
-
+//         if (--timer < 0) {
+//             timer = duration;
+//         }
+//     }, 1000);
 // }
+
+    var sec = 29;
+    var displayTime = document.querySelector("#timer");
+    
+
+    setInterval(function() {
+        displayTime.textContent = sec;
+    
+      sec--;
+      if(sec == 0)
+      {
+        sec = 30;
+      }
+     }, 1000);
+  
+     myLoop ();
+
+// window.onload = function () {
+//     var startCount = 5;
+//     var displayTime = document.querySelector("#timer");
+//     startTimer(startCount, displayTime);
+//     myLoop ();
+// };
+
+
+
+
+
 
 
 
